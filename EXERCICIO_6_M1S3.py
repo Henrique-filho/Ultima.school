f5f1b1cf627c2b966a4da3f1ccf3418478b46621
Henrique-filho <henrique_f.filho@outlook.com>
2024-03-05 22:06:20 -0300

    Create EXERCICIO_6_M1S3.py

---
+++ EXERCICIO_6_M1S3.py
@@ -0,0 +1,5 @@
+comanda = float(input('digite o valor da comanda: '))
+taxa = float(10)
+gorjeta = comanda*(taxa/100)
+print(f"Valor da Comanda: R$ {comanda:.2f}")
+print(f"gorjeta: R$ {gorjeta:.4f}")
